b44ed6696fc18ad4e15fd15f45b987302f69e7c1
Sean <niezsean@gmail.com>
2025-09-30 22:58:08 -0400

    added test script for API keys

---
+++ agent/README.md
@@ -0,0 +1,7 @@
+Project Overview  
+  
+This project seeks to design an intelligent data collection agent that leverages APIs dealing  
+with economic and stock data. In this project, 2 APIs are used (in accordance with the DMP)  
+those are the FRED API from the Federal Reserve, as well as the Alpha Vantage API.  
+  
+The config file houses user-defined information that is crucial to the agent's functionality. 
--- agent/tests/api_test_script.py
+++ agent/tests/api_test_script.py
@@ -1,2 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+FRED_API_KEY = os.getenv("FRED_API_KEY")
+ALPHA_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 
+# test FRED API
+
+fred_request = requests.get(
+    "https://api.stlouisfed.org/fred/series/observations",
+    params={
+        "series_id":"GDP",
+        "api_key":FRED_API_KEY,
+        "file_type": "json",
+        "observation_start":"2020-01-01",
+        "observation_end":"2024-12-25"
+    }
+)
+
+print(f"FRED key test: {fred_request.status_code == 200}") # if status code is 200, then they key is valid
+
+alpha_request = requests.get(
+    "https://www.alphavantage.co/query",
+    params = {
+        "function":"TIME_SERIES_DAILY",
+        "symbol":"MSFT",
+        "apikey":ALPHA_API_KEY,
+        "outputsize":"compact"
+    }
+)
+
+print(f"Alpha Vantage key test: {alpha_request.status_code == 200}")
